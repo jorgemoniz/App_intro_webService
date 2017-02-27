f705e6cd0fe342f84a96a56441d38fbd82353b57
Jorge Moñiz <jorgemoniz@live.com>
2017-02-27 20:40:29 +0100

    Initial Commit

---
+++ App_intro_webService/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App_intro_webService
+//
+//  Created by cice on 27/2/17.
+//  Copyright © 2017 jorgemoniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
