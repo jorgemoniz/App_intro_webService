f2159d285174d5794f959143d169fc0d7c03637f
Jorge Moñiz <jorgemoniz@live.com>
2017-02-27 21:42:02 +0100

    Revert "Primer commot inicial / instalación de Poda"
    This reverts commit 4a25bf7bc430e4d9ad34c246e454089c2cbd4b82.

--- App_intro_webService/ViewController.swift
+++ App_intro_webService/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  App_intro_webService
 //
-//  Created by Jorge Moñiz on 27/2/17.
+//  Created by cice on 27/2/17.
 //  Copyright © 2017 jorgemoniz. All rights reserved.
 //
 
--- Podfile
+++
@@ -1,7 +0,0 @@
-source 'https://github.com/CocoaPods/Specs.git'
-platform :ios, '10.0'
-use_frameworks!
-
-target 'App_intro_webService' do
-    pod 'SwiftyJSON'
-end
